fc6769d5e091e72bcb7e675873596adc6f32e3e8
pallavi3t <pallavi3t@gmail.com>
2023-02-27 16:21:58 -0800

    commenting and convention changes

--- data_generator.py
+++ data_generator.py
@@ -2,17 +2,25 @@ from faker import Faker
 from faker.providers import BaseProvider
 from math import trunc
 import random
+import csv
+import pandas
 
 fake = Faker()
 
-class CustomProvider(BaseProvider):
 
-    # # random generation of budget priorty 
-    # def budget_priority(self):
-    #     return random.choice(["high", "medium", "low"])
-    
-    # random generation of weighted income by percent distribution
-    def monthly_income(self):
+class IncomeProvider(BaseProvider):
+
+    def annual_income(self):
+        """
+        input: None
+        output: returns a random income in a range based off of weighted 
+        probabilities for each range
+
+        Based off of statistics given by:
+        - https://www.statista.com/statistics/203183/percentage-
+        distribution-of-household-income-in-the-us/
+        - https://dqydj.com/average-median-top-household-income-percentiles/
+        """
         x = fake.pyfloat(positive=True, min_value=1, max_value=100)
         generated_income = 0
 
@@ -40,14 +48,52 @@ class CustomProvider(BaseProvider):
             #ignoring the top 0.1% of earners
             generated_income = fake.random_int(830,0000,3212486)
         
-        return trunc(generated_income/12)
+        return generated_income
 
-fake.add_provider(CustomProvider)
-print("Monthly Income:", fake.monthly_income())
+"""
+generates weighted random income using annual_income func
+"""
+fake.add_provider(IncomeProvider)
+annual_income = fake.annual_income()
+print('Annual Income:', annual_income)
 
-# cost of living index 
-print("Cost of Living Index:", fake.random_int(85,184))
+# converting annual to monthly income 
+monthly_income = trunc(annual_income/12)
+print('Monthly Income:', monthly_income)
 
-budget_categories = ["Housing & Utility", "Transportation", "Food", "Healthcare", "Savings, Investments & Debt Payments", "Personal Spending"]
+
+"""
+generates random location and associated:
+    - cost of living index
+    - rent index
+    - food index
+from numbeo_col.csv
+"""
+df = pandas.read_csv('/Users/pallavitangirala/Documents/projects'
+                     '/budget-recommender/data/numbeo_col.csv')
+
+# generating random row number
+rand = fake.random_int(0, len(df.index))
+
+print('Location:', df.loc[rand, 'City'])
+
+print('Cost of Living Index:', 
+      df.loc[rand, 'Cost of Living Index'])
+
+print('Rent Index:', df.loc[rand, 'Rent Index'])
+
+# food index calculated as average of groceries and restaurant index 
+food_index = (df.loc[rand, 'Groceries Index'] 
+              + df.loc[rand, 'Restaurant Price Index']) / 2
+print('Food Index:', food_index)
+
+
+"""
+budget priority randomly ranked from 1 to 3 
+"""
+budget_categories = ['Housing & Utility', 'Transportation',
+                      'Food', 'Healthcare',
+                        'Savings, Investments & Debt Payments',
+                          'Personal Spending']
 for i in range(0,6):
     print(budget_categories[i],":", fake.random_int(1,3))
